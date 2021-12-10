831d772ba714be01a7358bd08f14c329c12246e2
Otto Jones <ottosjonesjr@gmail.com>
2021-12-10 15:54:28 +0300

    added variables on lines 5 and 6

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const computerChoiceDisplay = document.getElementById("computer-choice")
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultsDisplay = document.getElementById('results')
 const possibleChoices = document.querySelectorAll("button")
-// let userChoice
-// let results
+let userChoice
+let results
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener("click",  (e) => {
    userChoice = e.target.id 
